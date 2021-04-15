1fc68a97d9b52d311aed6e6584d4b53342b6f676
xnx3 <mail@xnx3.com>
2021-04-15 09:57:04 +0800

    update version

--- src/main/java/com/xnx3/yunbackups/visualApp/Global.java
+++ src/main/java/com/xnx3/yunbackups/visualApp/Global.java
@@ -19,7 +19,7 @@ public class Global {
 	public static LogJPanel logJPanel;
 	
 	//当前桌面app的版本
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.3";
 	//版本更新url
 	public static final String VERSION_CHECK_URL = "http://version.xnx3.com/yunbackups.html";
 	
